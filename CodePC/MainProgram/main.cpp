fac0608414706244690ca7e3f8e06d07a0c54261
AIlhomov <ilhomovvahid@gmail.com>
2023-12-21 23:33:20 +0100

    Added start and quit to menu

--- CodePC/MainProgram/main.cpp
+++ CodePC/MainProgram/main.cpp
@@ -35,16 +35,35 @@ int main()
 	menuText.setStyle(sf::Text::Bold);
 	menuText.setPosition(90.0f, 100.0f);
 
-	//Hint: "Press Space to Start"
+	////Hint: "Press Space to Start"
 
-	sf::Text menuHint;
-	menuHint.setFont(font);
-	menuHint.setString("Press Space to Start");
-	menuHint.setCharacterSize(20);
-	menuHint.setFillColor(sf::Color::Green);
-	menuHint.setStyle(sf::Text::Italic);
-	menuHint.setPosition(169.0f, 400.0f);
+	//sf::Text menuHint;
+	//menuHint.setFont(font);
+	//menuHint.setString("Press Space to Start");
+	//menuHint.setCharacterSize(20);
+	//menuHint.setFillColor(sf::Color::Green);
+	//menuHint.setStyle(sf::Text::Italic);
+	//menuHint.setPosition(169.0f, 400.0f);
 	//
+	 // Create "Start" text
+	sf::Text startText;
+	startText.setFont(font);
+	startText.setString("Start Game");
+	startText.setCharacterSize(24);
+	startText.setFillColor(sf::Color::Green);
+	startText.setStyle(sf::Text::Bold);
+	startText.setPosition(200.0f, 250.0f);
+
+	// Create "Quit" text
+	sf::Text quitText;
+	quitText.setFont(font);
+	quitText.setString("Quit");
+	quitText.setCharacterSize(24);
+	quitText.setFillColor(sf::Color::Green);
+	quitText.setStyle(sf::Text::Bold);
+	quitText.setPosition(250.0f, 300.0f);
+
+	int selectedOption = 0;  // 0 for "Start", 1 for "Quit"
 
 	while (menuWindow.isOpen())
 	{
@@ -53,9 +72,24 @@ int main()
 			switch (ev.type)
 			{
 			case sf::Event::KeyPressed:
-				if (ev.key.code == sf::Keyboard::Space) {
+				/*if (ev.key.code == sf::Keyboard::Space) {
 					menuWindow.close();
 					allowGame = true;
+				}*/
+				if (ev.key.code == sf::Keyboard::Up) {
+					selectedOption = (selectedOption - 1 + 2) % 2;
+				}
+				else if (ev.key.code == sf::Keyboard::Down) {
+					selectedOption = (selectedOption + 1) % 2;
+				}
+				else if (ev.key.code == sf::Keyboard::Enter) {
+					if (selectedOption == 0) {
+						menuWindow.close();
+						allowGame = true;
+					}
+					else if (selectedOption == 1) {
+						menuWindow.close();
+					}
 				}
 				break;
 			default:
@@ -63,8 +97,16 @@ int main()
 			}
 		}
 		menuWindow.clear(sf::Color(0,0,0));
+
+		// Highlight the selected option
+		startText.setFillColor(selectedOption == 0 ? sf::Color::Yellow : sf::Color::Green);
+		quitText.setFillColor(selectedOption == 1 ? sf::Color::Yellow : sf::Color::Green);
+
+		menuWindow.draw(startText);
+		menuWindow.draw(quitText);
+
 		menuWindow.draw(menuText);
-		menuWindow.draw(menuHint);
+		//menuWindow.draw(menuHint);
 		menuWindow.display();
 
 	}
