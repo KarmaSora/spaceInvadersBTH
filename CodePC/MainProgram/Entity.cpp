c79ac98848978c7ce1b45613a657c2ff01d0653c
KarmaSora <karammtr@gmail.com>
2024-01-08 15:21:36 +0100

    Removed unneccery virutal from entity

--- CodePC/MainProgram/Entity.cpp
+++ CodePC/MainProgram/Entity.cpp
@@ -6,7 +6,6 @@ void Entity::moveRectShape(float x, float y)
 }
 
 Entity::Entity()
-	//: alive(false), speed(10), xPos(400.f), yPos(400.f), dX(0), dY(0), widthHeight(sf::VideoMode::getDesktopMode().width, sf::VideoMode::getDesktopMode().height)
     : texturePath("../../CodePC/Images/BlueBalloon.png"), xPos(400.f), yPos(532.f), speed(5), alive(false), dX(0), dY(0), widthHeight(900, 600)
 
 {
--- CodePC/MainProgram/Entity.h
+++ CodePC/MainProgram/Entity.h
@@ -27,8 +27,6 @@ private:
 public:
     // Constructor and other member functions...
     Entity();
-    //Entity(sf::Texture texture, int xPos, int yPos, int speed, int dX, int dY, bool alive);
-    //Entity(sf::Texture texture, float xPos, float yPos, int speed, bool alive, int dX, int dY, float windowWidth, float windowHeight);
     Entity(std::string texturePath, float xPos, float yPos, int speed, bool alive, int dX, int dY, float windowWidth, float windowHeight);
    
 
@@ -41,49 +39,44 @@ public:
     //Setters&Getters
         // Setters
         // Setters for position
-    virtual void setDX(const int dX);
-    virtual void setDY(const int dY);
+    void setDX(const int dX);
+    void setDY(const int dY);
     virtual void setPosition(float xPos, float yPos);
-    virtual void setXPos(float xPos);
-    virtual void setYPos(float yPos);
-     void setTexture(const sf::Texture& texture);
-    virtual void setSpeed(int newSpeed);
-    virtual void setAlive(bool isAlive);
-    //virtual void setSprite(const sf::Sprite& newSprite);
-    //virtual void setPosOfSprite(float xPos, float yPos);
-    virtual void setPosOfRect(float xPos, float yPos);
+    void setXPos(float xPos);
+    void setYPos(float yPos);
+    void setTexture(const sf::Texture& texture);
+    void setSpeed(int newSpeed);
+    void setAlive(bool isAlive);
+    void setPosOfRect(float xPos, float yPos);
 
     
         // Setters for widthHeight
-    virtual void setWidth(float width);
-    virtual void setHeight(float height);
+    void setWidth(float width);
+    void setHeight(float height);
 
 
         // Getters
-   virtual int getDX() const;
-   virtual int getDY() const;
-   virtual float getXPos() const;
-   virtual float getYPos() const;
-   virtual const sf::Texture& getTexture() const;
-   virtual int getSpeed() const;
+    int getDX() const;
+    int getDY() const;
+    float getXPos() const;
+    float getYPos() const;
+    const sf::Texture& getTexture() const;
+    int getSpeed() const;
 
-   virtual bool isAlive() const;
-   //virtual const sf::Sprite& getSprite() const;
+    bool isAlive() const;
 
     // Getters for widthHeight
-    virtual sf::Vector2f getWidthHeight() const;
-    virtual float getWidth() const;
-    virtual float getHeight() const;
+     sf::Vector2f getWidthHeight() const;
+     float getWidth() const;
+     float getHeight() const;
 
-     //virtual float getPosXOfSprite();
-     virtual float getPosXOfRect();
-     //virtual float getPosYOfSprite();
-     virtual float getPosYOfRect();
+      float getPosXOfRect();
+      float getPosYOfRect();
 
     // Getters for position
-    virtual sf::Vector2f getPosition() const;
+     virtual sf::Vector2f getPosition() const;
 
-    virtual void draw(sf::RenderTarget& target, sf::RenderStates states) const override;
+     void draw(sf::RenderTarget& target, sf::RenderStates states) const override;
 };
 
 #endif
